6bf64bf1fea2c474a4bc873ac0f0157011253776
MadarsPodnieks <madars.podnieks@grobinasskola.lv>
2024-12-04 14:27:51 +0200

    izlabiju1

--- 02/main.py
+++ 02/main.py
@@ -1,24 +1,31 @@
 """ Method 1 """
-
+#ievadīt skaitļus
 num = int(input("Give me a number to check: "))
 check = int(input("Give me a number to divide by: "))
-
+#noosaka vai pirmais ievadītas skaitlis dalās ar 4 bez atlikuma
 if num % 4 == 0:
     print(num, "is a multiple of 4")
+#nosaka vai pirmais ievadītais skaitlis ir pāra skaitlis
 elif num % 2 == 0:
     print(num, "is an even number")
+#Ja pirmais ievad skaitlis nedalās ar 4 vai 2 bez atlikuma, tad tas ir nepāra skaitlis
 else:
     print(num, "is an odd number")
-
+#Noskaidro vai pirmais skaitlis dalās ar otro bez atlikuma
 if num % check == 0:
     print(num, "divides evenly by", check)
+#Ja nedalās bez atlikuma
 else:
     print(num, "doesn't divide evenly by", check)
 
 """ Method 2 """
+#ievada skaitli
 num = int(input("Enter a number: "))
+#"Mod" ir skaitļa dalījums ar 2 atlikums
 mod = num % 2
+#Ja skaitli izdalot ar 2 ir atlikums, tad izprintē, ka tas ir nepāra skaitlis
 if mod > 0:
     print("You picked an odd number.")
+#Ja nē, tad izprintē, ka tas ir pāra skaitlis
 else:
     print("You picked an even number.")
--- 03/main.py
+++ 03/main.py
@@ -1,10 +1,12 @@
+#dots saraksts
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
+#Ievda skaitli
 num = int(input("Choose a number: "))
-
+#Jauns saraksts
 new_list = []
-
+#Cik simboli ir iekšā "a" sarakstā
 for i in a:
+#Ja simbolu skaits ir mazāks
     if i < num:
         new_list.append(i)
 
